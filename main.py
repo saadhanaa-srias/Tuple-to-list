5196dca7b9ab8b08eb062667628a0bc0822ddff7
saadhanaa-srias <saadhanaasrias@gmail.com>
2024-09-24 17:46:46 +0530

    L1

---
+++ main.py
@@ -0,0 +1,23 @@
+my_tuple1 = ("First name:saadhanaa")
+print(my_tuple1)
+
+my_tuple2=("Last name:sri")
+print(my_tuple2)
+
+my_tuple3=("Age:16")
+print(my_tuple3)
+
+my_tuple4=("Favourite subject:computerscience")
+print(my_tuple4)
+ 
+my_tuple=list(my_tuple1)
+print(my_tuple)
+
+my_tuple=list(my_tuple2)
+print(my_tuple)
+
+my_tuple=list(my_tuple3)
+print(my_tuple)
+
+my_tuple=list(my_tuple4)
+print(my_tuple)
